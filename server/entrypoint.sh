8880e119572ebb52e7853db0cb6662f587a95828
Achim Christ <acch@users.noreply.github.com>
2018-01-09 10:17:51 +0100

    Initial version

---
+++ server/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/ash
+
+# Check if environment is defined
+if [ "$AUTHORIZED_KEYS" == "none" ]
+then
+  echo "ERROR: Please provide your public SSH key(s) using the AUTHORIZED_KEYS environment variable!" >&2
+  exit 1
+fi
+
+# Add authorized keys
+IFS=$'\n'
+for key in $(echo $AUTHORIZED_KEYS | tr "," "\n")
+do
+  trimmed_key=$(echo $key | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+  echo "Adding authorized key \"${trimmed_key}\"..."
+  echo $trimmed_key >> /root/.ssh/authorized_keys
+done
+
+# Generate host keys if they do not exist
+ssh-keygen -A
+
+# Start SSH Daemon in foreground
+exec /usr/sbin/sshd -D -e "$@"
